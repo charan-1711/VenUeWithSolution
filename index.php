73e01a9e57807835a527f8e335bddb200680ec20
charan-1711 <charan.a1711@gmail.com>
2024-04-30 14:24:07 +0530

    added php connection

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Venue With Solution</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Micro+5+Charted&display=swap" rel="stylesheet">
+</head>
+<body class="index">
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">VenUWithSoln</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="venues.html">VENUES</a></li>
+                    <!-- <li><a href="services.html">SERVICES</a></li> -->
+                    <li><a href="book.html">BOOK</a></li>
+                    <li><a href="contact.html">CONTACT</a></li>
+                </ul>
+            </div>
+            <!-- <div class="search">
+                <input class="srch" type="search" name="" placeholder ="Type To text">
+                <a href="#"><button class="button">SEARCH</button></a>
+            </div> -->
+       </div>
+       <div class="content">
+            <h1>VENUES<br><span>FOR ALL YOUR</span><br>FULFILLMENT</BR></h1>
+            <p class="par"><b>Explore a variety of venues ideal for any event in one location. From <br>
+                 intimate gatherings to grand affairs,<br>
+                 discover the perfect setting for every occasion.</b></p>
+          <button class="btn"><a href="contact.html">VISIT US</a></button>
+          <!-- <div class="form">
+            <h2>Login Here</h2>
+            <h3>(For staff only)</h3>
+            <input type="email" name="email" placeholder="email">
+            <input type="password" name="" placeholder="password">
+            <button class="btn"><a href="#">Login</a></button>
+
+            <p class="link">Don't have an account<br>
+            <a href="#">Sign Up</a></p>
+            <p class="liw">Login with</p>
+        </div>   -->
+       </div>
+    </div>
+</body>
+</html>
